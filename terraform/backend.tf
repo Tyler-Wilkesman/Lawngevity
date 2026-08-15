d9d373731e4e2cceead088fd5fd0a7b36e724df7
Tyler Wilkesman <Tyler.wilkesman@outlook.com>
2026-08-15 13:35:07 -0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+  bucket         = "file-share-tf-state"
+  key            = "file-share-state.tfstate"
+  region         = "us-east-1"
+  use_lockfile = true
+  encrypt        = true
+  }
+}
