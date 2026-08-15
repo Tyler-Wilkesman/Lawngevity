6f96e87b922f1a9d57e96f71e5d5355caf926f9e
Tyler Wilkesman <Tyler.wilkesman@outlook.com>
2026-08-15 13:54:42 -0400

    Add output for CloudFront domain name

---
+++ terraform/outputs.tf
@@ -0,0 +1,6 @@
+#Defining outputs
+
+output "cloudfront_domain_name" {
+  value       = aws_cloudfront_distribution.website.domain_name
+  description = "The domain name of the CloudFront distribution"
+}
