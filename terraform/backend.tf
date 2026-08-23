d3031e0fcd010a1f89b0ce9165c6f48917ae68ea
Tyler Wilkesman <Tyler.wilkesman@outlook.com>
2026-08-23 13:14:58 -0400

    Update S3 bucket name for Terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-  bucket         = "lawngevity-tf-state"
+  bucket         = "lawngevity-landscaping-tf-state"
   key            = "lawngevit-website.tfstate"
   region         = "us-east-1"
   use_lockfile = true
