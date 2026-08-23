9f1a953f450fe8d3ac27e52e428ae17be5a95909
Tyler Wilkesman <Tyler.wilkesman@outlook.com>
2026-08-23 13:24:37 -0400

    Fix typo in S3 backend key name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
   bucket         = "lawngevity-landscaping-tf-state"
-  key            = "lawngevit-website.tfstate"
+  key            = "lawngevity-website.tfstate"
   region         = "us-east-1"
   use_lockfile = true
   encrypt        = true
