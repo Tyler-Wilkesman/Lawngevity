b893982124a8cd9187205023bfd2353eca4029d9
Tyler Wilkesman <Tyler.wilkesman@outlook.com>
2026-08-23 11:28:23 -0400

    Refactor S3 object resource to use dynamic fileset

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,12 +37,43 @@ resource "aws_s3_bucket_public_access_block" "access" {
 
 #add files to S3 
 
-resource "aws_s3_object" "home" {
+resource "aws_s3_object" "website_files" {
+
+  for_each = fileset(
+    "${path.module}/../src/frontend",
+    "**/*"
+  )
+
   bucket = aws_s3_bucket.create.id
-  key    = "create"
-  source = "${path.module}/../src/frontend/lawngevity.html"
-  acl = "private"
-  content_type = "text/html"
+
+  key = each.value
+
+  source = "${path.module}/../src/frontend/${each.value}"
+
+  etag = filemd5(
+    "${path.module}/../src/frontend/${each.value}"
+  )
+
+  content_type = lookup({
+    html = "text/html"
+    css  = "text/css"
+    js   = "application/javascript"
+    png  = "image/png"
+    jpg  = "image/jpeg"
+    jpeg = "image/jpeg"
+    svg  = "image/svg+xml"
+    webp = "image/webp"
+    ico  = "image/x-icon"
+  },
+  lower(
+    element(
+      split(".", each.value),
+      length(split(".", each.value)) - 1
+    )
+  ),
+  "application/octet-stream"
+  )
+
   depends_on = [
     aws_s3_bucket_public_access_block.access
   ]
